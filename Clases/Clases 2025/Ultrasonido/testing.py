272cc1e2562f00ad0c38e86d11e3e950b8197f7c
Mikel286 <aniamarinm@gmail.com>
2026-04-08 17:16:02 -0400

    1.2.2 None

--- Clases/Clases 2025/Ultrasonido/testing.py
+++ Clases/Clases 2025/Ultrasonido/testing.py
@@ -1,3 +1,21 @@
 from sensores import Ultrasonico
+from machine import Pin
+from time import sleep
 
-pass
+led = Pin(2, Pin.OUT)
+sensor = Ultrasonico()
+
+for _ in range(10):
+    
+    distancia = sensor.medir()
+    
+    if distancia < 400:
+        led.value(1)
+    else:
+        led.value(0)
+    sleep(1)
+        
+    
+    
+    
+    
